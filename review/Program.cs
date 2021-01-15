1cc79f26c370782b3eba7da79a8cb39d541a9687
sara8998 <77490883+sara8998@users.noreply.github.com>
2021-01-15 15:43:18 +0300

    Add files via upload

---
+++ review/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace birth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("please enter your name: ");
+             string name = Console.ReadLine();
+            Console.WriteLine("enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            DateTime today = DateTime.Today;
+            int birthyear = today.Year - age;
+            
+            Console.WriteLine("your name is " + name + " and your birthyear is " + birthyear + ".");
+            Console.ReadLine();
+        }
+    }
+}
